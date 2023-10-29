dbbc39cc14917e6a9deba77c9d50acee776943fd
ying <leftleft0813@gmail.com>
2023-10-29 12:27:28 +0800

    移除環境變量

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-  distDir: "build", // 输出到 "build" 目录
-};
+module.exports = nextConfig;
--- pages/firebase.js
+++ pages/firebase.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
+// require("dotenv").config();
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyB9ZQW-t3sy0zMVD7_vWcSLjVI87Qu9EZk",
+  authDomain: "accounting-app-9e557.firebaseapp.com",
+  projectId: "accounting-app-9e557",
+  storageBucket: "accounting-app-9e557.appspot.com",
+  messagingSenderId: "141964990774",
+  appId: "1:141964990774:web:70a1cc863dadbb881b7b97",
 };
 
 const app = initializeApp(firebaseConfig);
